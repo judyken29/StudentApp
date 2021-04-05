9092111aecd456605a9907dcc07771f4e19c5f87
judyken29 <81622421+judyken29@users.noreply.github.com>
2021-04-05 17:06:17 +0530

    Create StudentApp.java

---
+++ StudentApp.java
@@ -0,0 +1,23 @@
+package p2;
+import java.util.Scanner;
+
+public class StudentApp {
+
+	public static void main(String[] args) {
+		
+		System.out.println(" Enter ID, Name, English Marks and Math Marks");
+		int studentId, englishMarks, mathMarks;
+		String studentName;
+		Scanner sc = new Scanner(System.in);
+		studentId = sc.nextInt();
+		studentName = sc.next();
+		englishMarks = sc.nextInt();
+		mathMarks = sc.nextInt();
+		sc.close();
+		
+		Student s1 = new Student(studentName, studentId, englishMarks, mathMarks);
+		s1.calculateMarks();
+		s1.printDetails();
+	}
+
+}
